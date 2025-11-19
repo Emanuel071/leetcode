20d570ac4e21466bbf3cf1bb558211d9809f0f7a
Emanuel Calderon <emanuelcalderon071@gmail.com>
2025-11-18 19:59:12 -0500

    adding insert and clone graph

---
+++ cloneGraph/cloneGraph.py
@@ -0,0 +1,46 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution(object):    
+
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+
+        old_to_new = {}
+
+        def dfs(node):
+            if node in old_to_new:
+                return old_to_new[node]
+
+            copy = Node(node.val)
+            old_to_new[node] = copy
+
+            for neighbor in node.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+
+            return copy
+
+        return dfs(node)
+
+
+    
+if __name__ == "__main__":
+    sol = Solution()
+    # You can add test cases here to create a graph and test the cloneGraph method.
+    # For example:
+    node1 = Node(1)
+    # node2 = Node(2)
+    # node1.neighbors = [node2]
+    # node2.neighbors = [node1]
+    cloned_graph = sol.cloneGraph(node1)
+    pass
---
+++ insert/insert.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        merged = []
+        i = 0
+        # Add all intervals ending before newInterval starts
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            merged.append(intervals[i])
+            i += 1
+
+        # Merge overlapping intervals
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        merged.append(newInterval)
+
+        # Add remaining intervals
+        while i < len(intervals):
+            merged.append(intervals[i])
+            i += 1
+
+        return merged
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.insert([[1,3],[6,9]], [2,5]))  # Output: [[1,5],[6,9]]
+    print(sol.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))  # Output: [[1,2],[3,10],[12,16]]
+        
